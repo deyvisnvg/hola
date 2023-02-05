898f321fe8fc2cce21b7ce075ed75d5069d73790
deyvisnvg <deyvisnvg@gmail.com>
2023-02-05 01:33:46 -0500

    cambios

--- src/app.ts
+++ src/app.ts
@@ -2,21 +2,7 @@ import express, { type Application } from "express";
 import cors from "cors";
 import routes from "./routes";
 
-const { v4 } = require('uuid');
-
 const app: Application = express();
-
-app.get('/api', (req, res) => {
-    const path = `/api/item/${v4()}`;
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-app.get('/api/item/:slug', (req, res) => {
-    const { slug } = req.params;
-    res.end(`Item: ${slug}`);
-});
 
 app.use(express.static('public'));
 app.use(express.json());
